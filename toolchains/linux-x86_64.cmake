c278875be89ee6e26e4058e0e3b1b17d823e576b
destroyer <destroyer.dcf@gmail.com>
2025-10-11 11:57:53 +0200

    refactor

--- toolchains/linux-x86_64.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
-set(CMAKE_C_COMPILER /opt/homebrew/opt/x86_64-unknown-linux-gnu/bin/x86_64-unknown-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER /opt/homebrew/opt/x86_64-unknown-linux-gnu/bin/x86_64-unknown-linux-gnu-g++)
